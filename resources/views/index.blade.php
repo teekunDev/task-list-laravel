f2f06cf63bfe66eb6395f797c303db9033d6eecc
teekunDev <teekunsenpai@proton.me>
2023-11-22 14:55:36 +0100

    Added Layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,16 @@
-Hello I am a Blade Template
+@extends('layouts.app')
 
-@isset($name)
-    This is: {{ $name }}     
-@endisset
+@section('title', 'The list of tasks')
+
+@section('content')
+<div>
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}"> {{ $task->title }} </a>
+        </div>
+    @empty
+        <div>There are no Tasks!</div>
+    @endforelse
+
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Task List</title>
+    </head>
+    <body>
+        <h1>@yield('title')</h1>
+
+        <div>
+            @yield('content')
+        </div>
+    </body>
+</html>
